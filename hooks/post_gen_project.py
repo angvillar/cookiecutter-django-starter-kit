d62c4c322cba4f860e5a026a1ebf863c7d1188bb
angvillar <angvillar@gmail.com>
2019-06-14 17:09:04 +0200

    postgres local

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,114 @@
+"""
+NOTE:
+    the below code is to be maintained Python 2.x-compatible
+    as the whole Cookiecutter Django project initialization
+    can potentially be run in Python 2.x environment
+    (at least so we presume in `pre_gen_project.py`).
+TODO: ? restrict Cookiecutter Django project initialization to Python 3.x environments only
+"""
+from __future__ import print_function
+
+import os
+import random
+import string
+
+try:
+    # Inspired by
+    # https://github.com/django/django/blob/master/django/utils/crypto.py
+    random = random.SystemRandom()
+    using_sysrandom = True
+except NotImplementedError:
+    using_sysrandom = False
+
+TERMINATOR = "\x1b[0m"
+SUCCESS = "\x1b[1;32m [SUCCESS]: "
+
+DEBUG_VALUE = "debug"
+
+
+def generate_random_string(
+        length, using_digits=False, using_ascii_letters=False, using_punctuation=False
+):
+    """
+    Example:
+        opting out for 50 symbol-long, [a-z][A-Z][0-9] string
+        would yield log_2((26+26+50)^50) ~= 334 bit strength.
+    """
+    if not using_sysrandom:
+        return None
+
+    symbols = []
+    if using_digits:
+        symbols += string.digits
+    if using_ascii_letters:
+        symbols += string.ascii_letters
+    if using_punctuation:
+        all_punctuation = set(string.punctuation)
+        # These symbols can cause issues in environment variables
+        unsuitable = {"'", '"', "\\", "$"}
+        suitable = all_punctuation.difference(unsuitable)
+        symbols += "".join(suitable)
+    return "".join([random.choice(symbols) for _ in range(length)])
+
+
+def set_flag(file_path, flag, value=None, formatted=None, *args, **kwargs):
+    if value is None:
+        random_string = generate_random_string(*args, **kwargs)
+        if random_string is None:
+            print(
+                "We couldn't find a secure pseudo-random number generator on your system. "
+                "Please, make sure to manually {} later.".format(flag)
+            )
+            random_string = flag
+        if formatted is not None:
+            random_string = formatted.format(random_string)
+        value = random_string
+
+    with open(file_path, "r+") as f:
+        file_contents = f.read().replace(flag, value)
+        f.seek(0)
+        f.write(file_contents)
+        f.truncate()
+
+    return value
+
+
+def generate_random_user():
+    return generate_random_string(length=32, using_ascii_letters=True)
+
+
+def generate_postgres_user(debug=False):
+    return DEBUG_VALUE if debug else generate_random_user()
+
+
+def set_postgres_user(file_path, value):
+    postgres_user = set_flag(file_path, "!!!SET POSTGRES_USER!!!", value=value)
+    return postgres_user
+
+
+def set_postgres_password(file_path, value=None):
+    postgres_password = set_flag(
+        file_path,
+        "!!!SET POSTGRES_PASSWORD!!!",
+        value=value,
+        length=64,
+        using_digits=True,
+        using_ascii_letters=True,
+    )
+    return postgres_password
+
+
+def set_flags_in_envs(postgres_user):
+    local_postgres_envs_path = os.path.join(".envs", ".local", ".postgres")
+
+    set_postgres_user(local_postgres_envs_path, postgres_user)
+    set_postgres_password(local_postgres_envs_path)
+
+
+def main():
+    set_flags_in_envs(generate_random_user())
+    print(SUCCESS + "Project initialized, keep up the good work!" + TERMINATOR)
+
+
+if __name__ == "__main__":
+    main()
